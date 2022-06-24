4871703253cc4367fb838ee5e72ee4fcf760c391
DaniMartinezMarin <martinez.marin.dani>
2022-06-24 10:46:17 +0200

    feat: Pipes i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
@@ -18,6 +19,7 @@ registerLocaleData( localeFr );
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRouterModule,
     SharedModule,
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css']
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  nombre: string = 'Dani';
+  genero: string = 'masculino';
+  textoChicoMapping= {
+    'masculino': 'un chico',
+    'femenino': 'una chica'
+  };
 
-  ngOnInit(): void {
+  clientes: string[] = ['Dani', 'Gema', 'Juan', 'Pepe'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 
+  cambiarCliente(): void {
+    if (this.genero === 'masculino')
+    {
+      this.genero = 'femenino';
+      this.nombre = 'Gema';
+    }else {
+      this.genero = 'masculino';
+      this.nombre = 'Dani';
+    }
+  }
+
+  borrarCliente(): void {
+    this.clientes.pop();
+  }
 }
