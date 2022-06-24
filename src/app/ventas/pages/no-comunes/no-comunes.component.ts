3d332a99b3cad5cc18ee73687884d07ee1e78e24
DaniMartinezMarin <martinez.marin.dani>
2022-06-24 11:12:26 +0200

    feat: KeyValue y Json pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,30 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styleUrls: ['./no-comunes.component.css']
+  styleUrls: ['./no-comunes.component.css'],
 })
 export class NoComunesComponent {
-
+  //Select pipe
   nombre: string = 'Dani';
   genero: string = 'masculino';
-  textoChicoMapping= {
-    'masculino': 'un chico',
-    'femenino': 'una chica'
+  textoChicoMapping = {
+    masculino: 'un chico',
+    femenino: 'una chica',
   };
 
+  //Plural pipe
   clientes: string[] = ['Dani', 'Gema', 'Juan', 'Pepe'];
   clientesMap = {
     '=0': 'no tenemos ningun cliente esperando.',
     '=1': 'tenemos un cliente esperando.',
-    'other': 'tenemos # clientes esperando.'
-  }
+    other: 'tenemos # clientes esperando.',
+  };
 
   cambiarCliente(): void {
-    if (this.genero === 'masculino')
-    {
+    if (this.genero === 'masculino') {
       this.genero = 'femenino';
       this.nombre = 'Gema';
-    }else {
+    } else {
       this.genero = 'masculino';
       this.nombre = 'Dani';
     }
@@ -35,4 +35,27 @@ export class NoComunesComponent {
   borrarCliente(): void {
     this.clientes.pop();
   }
+
+  //Keyvalue pipe
+  persona = {
+    nombre: 'Dani',
+    edad: 26,
+    ciudad: 'Valencia',
+  };
+
+  //Json pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Batman',
+      vuela: true
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    }
+  ];
 }
